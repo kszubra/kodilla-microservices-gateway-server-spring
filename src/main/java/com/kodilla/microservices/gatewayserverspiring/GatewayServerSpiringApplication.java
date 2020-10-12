e737dc1360f576b615262ce55727c350b63e415c
kacper <kacper.szubra>
2020-10-12 17:10:06 +0200

    first commit

---
+++ src/main/java/com/kodilla/microservices/gatewayserverspiring/GatewayServerSpiringApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.microservices.gatewayserverspiring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServerSpiringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerSpiringApplication.class, args);
+    }
+
+}
